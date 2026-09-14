52026d55c64b13621f443af54d877b6a945f9d44
javokhir-nw <javokhirganiboyev2004@gmail.com>
2026-09-14 18:08:52 +0500

    mvp

--- src/services/api.js
+++ src/services/api.js
@@ -5,6 +5,7 @@ import { useAuthStore } from '../stores/auth'
 const api = axios.create({
     baseURL: import.meta.env.VITE_API_URL,
     headers: { 'Content-Type': 'application/json' },
+    withCredentials: true,
 })
 
 api.interceptors.request.use((config) => {
